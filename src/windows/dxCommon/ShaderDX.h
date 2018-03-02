38a46508aa21cd40e8b6c1bb8206b7aaffb98d93
CaptainJH <heqi.ju@outlook.com>
2018-03-02 14:38:17 +0800

    update comments for class ShaderDX

--- src/windows/dxCommon/ShaderDX.h
+++ src/windows/dxCommon/ShaderDX.h
@@ -1,26 +1,7 @@
-//--------------------------------------------------------------------------------
-// This file is a portion of the Hieroglyph 3 Rendering Engine.  It is distributed
-// under the MIT License, available in the root of this distribution and 
-// at the following URL:
-//
-// http://www.opensource.org/licenses/mit-license.php
-//
-// Copyright (c) Jason Zink 
-//--------------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------------
-// ShaderDX11
-//
-// The shader base class provides the mechanics of linking state information to
-// a given shader.  Each type of shader utilizes various resources which are 
-// managed by the renderer.  The specific resources are determined at shader load
-// time via the reflection API and stored in the lists shown below.
-//
-// When a shader is bound to the pipeline, it's needed resources are updated if
-// needed and then loaded into the corresponding shader stage.
-//--------------------------------------------------------------------------------
-#ifndef ShaderDX11_h
-#define ShaderDX11_h
+//***************************************************************************************
+// ShaderDX.h by Heqi Ju (C) 2018 All Rights Reserved.
+//***************************************************************************************
+#pragma once
 //--------------------------------------------------------------------------------
 #include "PCH.h"
 #include "dxCommon/d3dUtil.h"
@@ -78,7 +59,5 @@ namespace forward
 		//ShaderReflectionDX11*					m_pReflection;
 	};
 };
-//--------------------------------------------------------------------------------
-#endif // ShaderDX11_h
 //--------------------------------------------------------------------------------
 
