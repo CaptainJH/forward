ecb1e1b442522d17405765071ea7ee464b566df9
CaptainJH <heqi.ju@outlook.com>
2018-08-11 09:44:56 +0800

    add grid and cylinder geometry builder

--- src/render/FrameGraph/Geometry.h
+++ src/render/FrameGraph/Geometry.h
@@ -226,7 +226,7 @@ namespace forward
 		/// Creates a sphere centered at the origin with the given radius.  The
 		/// slices and stacks parameters control the degree of tessellation.
 		///</summary>
-		GeometryBuilder<GeometryPrefab::GP_SPHERE>::GeometryBuilder(f32 radius, u32 sliceCount, u32 stackCount)
+		GeometryBuilder(f32 radius, u32 sliceCount, u32 stackCount)
 		{
 			//
 			// Compute the vertices stating at the top pole and moving down the stacks.
@@ -356,6 +356,328 @@ namespace forward
 		u32 GetIndexCount() const
 		{
 			return static_cast<u32>(meshData.Indices32.size());
+		}
+	};
+
+	template<>
+	struct GeometryBuilder<GP_GRID>
+	{
+		MeshData<Vertex_P_N_T_UV> meshData;
+
+		///<summary>
+		/// Creates an mxn grid in the xz-plane with m rows and n columns, centered
+		/// at the origin with the specified width and depth.
+		///</summary>
+		GeometryBuilder(f32 width, f32 depth, u32 m, u32 n)
+		{
+			u32 vertexCount = m * n;
+			u32 faceCount = (m - 1)*(n - 1) * 2;
+
+			//
+			// Create the vertices.
+			//
+
+			f32 halfWidth = 0.5f*width;
+			f32 halfDepth = 0.5f*depth;
+
+			f32 dx = width / (n - 1);
+			f32 dz = depth / (m - 1);
+
+			f32 du = 1.0f / (n - 1);
+			f32 dv = 1.0f / (m - 1);
+
+			meshData.Vertices.resize(vertexCount);
+			for (u32 i = 0; i < m; ++i)
+			{
+				f32 z = halfDepth - i * dz;
+				for (u32 j = 0; j < n; ++j)
+				{
+					f32 x = -halfWidth + j * dx;
+
+					meshData.Vertices[i*n + j].Pos = { x, 0.0f, z };
+					meshData.Vertices[i*n + j].Normal = { 0.0f, 1.0f, 0.0f };
+					meshData.Vertices[i*n + j].Tangent = { 1.0f, 0.0f, 0.0f };
+
+					// Stretch texture over grid.
+					meshData.Vertices[i*n + j].UV.x = j * du;
+					meshData.Vertices[i*n + j].UV.y = i * dv;
+				}
+			}
+
+			//
+			// Create the indices.
+			//
+
+			meshData.Indices32.resize(faceCount * 3); // 3 indices per face
+
+			// Iterate over each quad and compute indices.
+			u32 k = 0;
+			for (u32 i = 0; i < m - 1; ++i)
+			{
+				for (u32 j = 0; j < n - 1; ++j)
+				{
+					meshData.Indices32[k] = i * n + j;
+					meshData.Indices32[k + 1] = i * n + j + 1;
+					meshData.Indices32[k + 2] = (i + 1)*n + j;
+
+					meshData.Indices32[k + 3] = (i + 1)*n + j;
+					meshData.Indices32[k + 4] = i * n + j + 1;
+					meshData.Indices32[k + 5] = (i + 1)*n + j + 1;
+
+					k += 6; // next quad
+				}
+			}
+		}
+
+		void Initializer(forward::SimpleGeometry* geometry)
+		{
+			for (auto& v : meshData.Vertices)
+			{
+				geometry->AddVertex(v);
+			}
+
+			for (auto i = 0U; i < meshData.FaceCount(); ++i)
+			{
+				geometry->AddFace(TriangleIndices(
+					meshData.Indices32[i * 3 + 0],
+					meshData.Indices32[i * 3 + 1],
+					meshData.Indices32[i * 3 + 2]));
+			}
+		}
+
+		u32 GetVertexCount() const
+		{
+			return static_cast<u32>(meshData.Vertices.size());
+		}
+
+		u32 GetIndexCount() const
+		{
+			return static_cast<u32>(meshData.Indices32.size());
+		}
+
+	};
+
+	template<>
+	struct GeometryBuilder<GP_CYLINDER>
+	{
+		MeshData<Vertex_P_N_T_UV> meshData;
+
+		typedef Vertex_P_N_T_UV VertexType;
+		typedef MeshData<VertexType> MeshDataType;
+
+		///<summary>
+		/// Creates a cylinder parallel to the y-axis, and centered about the origin.  
+		/// The bottom and top radius can vary to form various cone shapes rather than true
+		/// cylinders.  The slices and stacks parameters control the degree of tessellation.
+		///</summary>
+		GeometryBuilder(f32 bottomRadius, f32 topRadius, f32 height, u32 sliceCount, u32 stackCount)
+		{
+			//
+			// Build Stacks.
+			// 
+
+			f32 stackHeight = height / stackCount;
+
+			// Amount to increment radius as we move up each stack level from bottom to top.
+			f32 radiusStep = (topRadius - bottomRadius) / stackCount;
+
+			u32 ringCount = stackCount + 1;
+
+			// Compute vertices for each stack ring starting at the bottom and moving up.
+			for (u32 i = 0U; i < ringCount; ++i)
+			{
+				f32 y = -0.5f*height + i * stackHeight;
+				f32 r = bottomRadius + i * radiusStep;
+
+				// vertices of ring
+				f32 dTheta = 2.0f*f_PI / sliceCount;
+				for (u32 j = 0; j <= sliceCount; ++j)
+				{
+					VertexType vertex;
+
+					f32 c = cosf(j*dTheta);
+					f32 s = sinf(j*dTheta);
+
+					vertex.Pos = { r*c, y, r*s };
+
+					vertex.UV.x = (f32)j / sliceCount;
+					vertex.UV.y = 1.0f - (f32)i / stackCount;
+
+					// Cylinder can be parameterized as follows, where we introduce v
+					// parameter that goes in the same direction as the v tex-coord
+					// so that the bitangent goes in the same direction as the v tex-coord.
+					//   Let r0 be the bottom radius and let r1 be the top radius.
+					//   y(v) = h - hv for v in [0,1].
+					//   r(v) = r1 + (r0-r1)v
+					//
+					//   x(t, v) = r(v)*cos(t)
+					//   y(t, v) = h - hv
+					//   z(t, v) = r(v)*sin(t)
+					// 
+					//  dx/dt = -r(v)*sin(t)
+					//  dy/dt = 0
+					//  dz/dt = +r(v)*cos(t)
+					//
+					//  dx/dv = (r0-r1)*cos(t)
+					//  dy/dv = -h
+					//  dz/dv = (r0-r1)*sin(t)
+
+					// This is unit length.
+					vertex.Tangent = { -s, 0.0f, c };
+
+					f32 dr = bottomRadius - topRadius;
+					Vector3f bitangent(dr*c, -height, dr*s);
+
+					auto T = vertex.Tangent;
+					auto B = bitangent;
+					auto N = T.Cross(B);
+					N.Normalize();
+					vertex.Normal = N;
+
+					meshData.Vertices.push_back(vertex);
+				}
+			}
+
+			// Add one because we duplicate the first and last vertex per ring
+			// since the texture coordinates are different.
+			u32 ringVertexCount = sliceCount + 1;
+
+			// Compute indices for each stack.
+			for (u32 i = 0; i < stackCount; ++i)
+			{
+				for (u32 j = 0; j < sliceCount; ++j)
+				{
+					meshData.Indices32.push_back(i*ringVertexCount + j);
+					meshData.Indices32.push_back((i + 1)*ringVertexCount + j);
+					meshData.Indices32.push_back((i + 1)*ringVertexCount + j + 1);
+
+					meshData.Indices32.push_back(i*ringVertexCount + j);
+					meshData.Indices32.push_back((i + 1)*ringVertexCount + j + 1);
+					meshData.Indices32.push_back(i*ringVertexCount + j + 1);
+				}
+			}
+
+			BuildCylinderTopCap(topRadius, height, sliceCount);
+			BuildCylinderBottomCap(bottomRadius, height, sliceCount);
+		}
+
+		void Initializer(forward::SimpleGeometry* geometry)
+		{
+			for (auto& v : meshData.Vertices)
+			{
+				geometry->AddVertex(v);
+			}
+
+			for (auto i = 0U; i < meshData.FaceCount(); ++i)
+			{
+				geometry->AddFace(TriangleIndices(
+					meshData.Indices32[i * 3 + 0],
+					meshData.Indices32[i * 3 + 1],
+					meshData.Indices32[i * 3 + 2]));
+			}
+		}
+
+		u32 GetVertexCount() const
+		{
+			return static_cast<u32>(meshData.Vertices.size());
+		}
+
+		u32 GetIndexCount() const
+		{
+			return static_cast<u32>(meshData.Indices32.size());
+		}
+
+		void BuildCylinderTopCap(f32 topRadius, f32 height, u32 sliceCount)
+		{
+			u32 baseIndex = (u32)meshData.Vertices.size();
+
+			float y = 0.5f*height;
+			float dTheta = 2.0f*f_PI / sliceCount;
+
+			// Duplicate cap ring vertices because the texture coordinates and normals differ.
+			for (u32 i = 0; i <= sliceCount; ++i)
+			{
+				float x = topRadius * cosf(i*dTheta);
+				float z = topRadius * sinf(i*dTheta);
+
+				// Scale down by the height to try and make top cap texture coord area
+				// proportional to base.
+				float u = x / height + 0.5f;
+				float v = z / height + 0.5f;
+
+				meshData.Vertices.push_back({ 
+					{ x, y, z }, 
+					{ 0.0f, 1.0f, 0.0f }, 
+					{ 1.0f, 0.0f, 0.0f }, 
+					{ u, v } 
+					});
+			}
+
+			// Cap center vertex.
+			meshData.Vertices.push_back({ 
+				{ 0.0f, y, 0.0f }, 
+				{ 0.0f, 1.0f, 0.0f }, 
+				{ 1.0f, 0.0f, 0.0f }, 
+				{ 0.5f, 0.5f } 
+				});
+
+			// Index of center vertex.
+			u32 centerIndex = (u32)meshData.Vertices.size() - 1;
+
+			for (u32 i = 0; i < sliceCount; ++i)
+			{
+				meshData.Indices32.push_back(centerIndex);
+				meshData.Indices32.push_back(baseIndex + i + 1);
+				meshData.Indices32.push_back(baseIndex + i);
+			}
+		}
+
+		void BuildCylinderBottomCap(f32 bottomRadius, f32 height, u32 sliceCount)
+		{
+			// 
+			// Build bottom cap.
+			//
+
+			u32 baseIndex = (u32)meshData.Vertices.size();
+			f32 y = -0.5f*height;
+
+			// vertices of ring
+			f32 dTheta = 2.0f*f_PI / sliceCount;
+			for (u32 i = 0; i <= sliceCount; ++i)
+			{
+				f32 x = bottomRadius * cosf(i*dTheta);
+				f32 z = bottomRadius * sinf(i*dTheta);
+
+				// Scale down by the height to try and make top cap texture coord area
+				// proportional to base.
+				f32 u = x / height + 0.5f;
+				f32 v = z / height + 0.5f;
+
+				meshData.Vertices.push_back({ 
+					{ x, y, z }, 
+					{ 0.0f, -1.0f, 0.0f }, 
+					{ 1.0f, 0.0f, 0.0f }, 
+					{ u, v } 
+					});
+			}
+
+			// Cap center vertex.
+			meshData.Vertices.push_back({ 
+				{ 0.0f, y, 0.0f }, 
+				{ 0.0f, -1.0f, 0.0f }, 
+				{ 1.0f, 0.0f, 0.0f }, 
+				{ 0.5f, 0.5f } 
+				});
+
+			// Cache the index of center vertex.
+			u32 centerIndex = (u32)meshData.Vertices.size() - 1;
+
+			for (u32 i = 0; i < sliceCount; ++i)
+			{
+				meshData.Indices32.push_back(centerIndex);
+				meshData.Indices32.push_back(baseIndex + i);
+				meshData.Indices32.push_back(baseIndex + i + 1);
+			}
 		}
 	};
 }
