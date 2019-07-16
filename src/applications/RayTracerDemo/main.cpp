d0f398937b5ec71602cffdbf71aba488ad2fa241
CaptainJH <heqi.ju@outlook.com>
2019-07-17 02:00:04 +0800

    save output image

--- src/applications/RayTracerDemo/main.cpp
+++ src/applications/RayTracerDemo/main.cpp
@@ -1,6 +1,7 @@
 #include "PCH.h"
 #include "Timer.h"
 #include "FileSystem.h"
+#include "FileSaver.h"
 #include "Log.h"
 #include "Utils.h"
 
@@ -17,6 +18,9 @@ using namespace forward;
 
 forward::FileSystem g_fileSystem;
 
+const i32 Width = 1600;
+const i32 Height = 900;
+
 i32 main()
 {
 	// Enable run-time memory check for debug builds.
@@ -24,4 +28,18 @@ i32 main()
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
+	u8* imageBuffer = new u8[Width * Height * 4];
+	u8* currentPixel = nullptr;
+	for (auto h = 0; h < Height; ++h)
+	{
+		for (auto w = 0; w < Width; ++w)
+		{
+			currentPixel = imageBuffer + h * Width * 4 + w * 4;
+			*(u32*)currentPixel = Colors::Blue.toARGB();
+		}
+	}
+
+	FileSaver outputFile;
+	outputFile.SaveAsBMP(L"RayTracerOutput.bmp", imageBuffer, Width, Height);
+	SAFE_DELETE_ARRAY(imageBuffer);
 }
