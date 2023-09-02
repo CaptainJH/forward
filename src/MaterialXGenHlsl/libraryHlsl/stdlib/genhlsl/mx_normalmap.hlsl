84d1f9d5db92a46dcac1abe95d31dab0c60b8972
Heqi Ju <heqi.ju@outlook.com>
2023-09-02 14:09:30 +0800

    update mx_normalmap

--- src/MaterialXGenHlsl/libraryHlsl/stdlib/genhlsl/mx_normalmap.hlsl
+++ src/MaterialXGenHlsl/libraryHlsl/stdlib/genhlsl/mx_normalmap.hlsl
@@ -1,7 +1,7 @@
 void mx_normalmap(float3 value, int map_space, float normal_scale, float3 N, float3 T,  out float3 result)
 {
     // Decode the normal map.
-    value = (value == float3(0.0f)) ? float3(0.0, 0.0, 1.0) : value * 2.0 - 1.0;
+    value = (value == (float3)0.0f) ? float3(0.0, 0.0, 1.0) : value * 2.0 - 1.0;
 
     // Transform from tangent space if needed.
     if (map_space == 0)
